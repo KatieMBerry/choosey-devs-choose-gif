f92a50b59c5e5dc26c6fd256f0051044a02233bc
kadycat <katieberrym@gmail.com>
2021-05-14 15:23:20 -0700

    Update giphyApi.js
    wanted a different default gif search term

--- src/services/giphyApi.js
+++ src/services/giphyApi.js
@@ -1,6 +1,6 @@
 // const fetch = require("node-fetch");
 
-export const searchGifs = (search = 'honey') => {
+export const searchGifs = (search = 'rainbow') => {
     return fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${search}&limit=16&offset=0&rating=pg&lang=en`)
         .then(res => res.json())
         .then(({ data }) => data.map(item => ({
