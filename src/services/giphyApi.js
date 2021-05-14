1cb408a39195a6c344fa41b75aa5b99b7b642e15
Katie <katieberrym@gmail.com>
2021-05-14 11:00:51 -0700

    added default search term and removed app snapshots and test

--- src/services/giphyApi.js
+++ src/services/giphyApi.js
@@ -1,6 +1,6 @@
 const fetch = require("node-fetch");
 
-export const searchGifs = search => {
+export const searchGifs = (search = 'honey') => {
     return fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${search}&limit=25&offset=0&rating=pg&lang=en`)
         .then(res => res.json())
         .then(({ data }) => data.map(({ item }) => ({
