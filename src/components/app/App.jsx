19e017dbc2f22e1696223279b4504b10cc0b4416
Katie <katieberrym@gmail.com>
2021-05-14 11:13:50 -0700

    installed react-router-dom and created routes for search page and favorites page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 import GIFSearch from '../../containers/GIFSearch';
+// import FavoritesList from '../../containers/FavoritesList';
 
 export default function App() {
   return (
     <>
-      <GIFSearch />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={GIFSearch} />
+          {/* <Route exact path="/favorites" component={FavoritesList} /> */}
+        </Switch>
+      </Router>
     </>
   );
 }
